a93b6b35f3a73fe1a0b55b5567549c0a5b7c600c
Harmandeep Singh <dhindsaharman1110@gmail.com>
2020-12-05 12:09:20 -0800

    Webpage is working properly and the text is displayed in the text area when user click on the list item

--- script/index.js
+++ script/index.js
@@ -65,11 +65,80 @@ function get_every_thing(){
 
 
 
-var notesArray=[]
 
 
 
-function save_notes(){
+
+// function save_notes(){
+//     let text_title=txt_area.value.split("\n")
+//     let temp_list=[]
+//     let string_value=""
+//     for (let items of text_title){
+        
+//         if (items==text_title[0]){
+//             temp_list.push(text_title[0])
+//         }
+//         else{
+//             string_value=string_value+ " " +items
+//         }
+    
+//     }
+
+
+
+
+//     let item_entry=document.querySelector('#list_items')
+//     let entry=document.createElement('li')
+//     let anchor_entry=document.createElement('a')
+//     let cls=document.createAttribute("class")
+//     cls.value="triggerClass"
+//     anchor_entry.setAttributeNode(cls)
+//     let att=document.createAttribute("href")
+//     att.value="#"
+//     anchor_entry.setAttributeNode(att)
+//     entry.appendChild(anchor_entry)
+//     anchor_entry.appendChild(document.createTextNode(temp_list[0]))
+//     item_entry.appendChild(entry)
+
+
+//     return temp_list
+
+
+
+
+
+
+// }
+
+
+
+
+// function make_array(){
+//     let notes_array=[]
+
+
+//     temp_list.push(string_value)
+//     let note_one={title: "note one" ,
+//                   field: "These are the first notes with title 'note one"}
+//     notesArray.push(note_one)
+
+//     let note_two={title: "note two" , field: "These are the second notes with title 'note two"}
+//     notesArray.push(note_two)
+
+//     let new_note={title: temp_list[0], field: temp_list[1]}
+//     notesArray.push(new_note)
+
+// }
+
+
+let notesArray=[{title:"note one", body:"some text 1"}, {title:"note two", body: "some text 2"}]
+
+
+
+
+
+function get_sorted_list(){
+
     let text_title=txt_area.value.split("\n")
     let temp_list=[]
     let string_value=""
@@ -85,17 +154,21 @@ function save_notes(){
     }
 
     temp_list.push(string_value)
-    let note_one={title: "note one" ,
-                  field: "These are the first notes with title 'note one"}
-    notesArray.push(note_one)
 
-    let note_two={title: "note two" , field: "These are the second notes with title 'note two"}
-    notesArray.push(note_two)
+    return temp_list
 
-    let new_note={title: temp_list[0], field: temp_list[1]}
-    notesArray.push(new_note)
+}
 
-    // txt_area.value=notesArray[2].field
+
+function save_notes(){
+
+    let temp_list=get_sorted_list()
+    if (temp_list[0] != ""){
+        notesArray.push({title: temp_list[0], body: temp_list[1]})
+    }
+
+    
+
     let item_entry=document.querySelector('#list_items')
     let entry=document.createElement('li')
     let anchor_entry=document.createElement('a')
@@ -108,22 +181,28 @@ function save_notes(){
     entry.appendChild(anchor_entry)
     anchor_entry.appendChild(document.createTextNode(temp_list[0]))
     item_entry.appendChild(entry)
+    
+
+    
+
+    document.querySelectorAll('.triggerClass').forEach(item => {
+        item.addEventListener('click', event => {
+            
+            for (notes of notesArray){
+                if (item.innerHTML == notes.title){
+                    txt_area.value=notes.body
+                }
+            }
+    
+        })
+      })
 
 
+    
+    
 
-
-
-    var elem =document.getElementsByClassName("triggerClass")
-    new_list=notesArray
-    console.log(new_list)
-    // console.log(elem.length)
-    for (var i=0; i<elem.length; i++){
-        elem[i].addEventListener("click", function(){
-            console.log(new_list[i])
-        }
-        )
-    }
 }
+
 
 
 
@@ -135,14 +214,19 @@ but_1.addEventListener("click",get_every_thing)
 
 but_3.addEventListener("click", save_notes)
 
-// nav_a.addEventListener("click", function noteone(){
-//     txt_area.value="These are the first notes with title 'note one'"
-// })
 
-// nav_a2.addEventListener("click", function notetwo(){
-//     txt_area.value="These are the second notes with title 'note two'"
-// })
+// document.querySelectorAll('.triggerClass').forEach(item => {
+//     item.addEventListener('click', event => {
+//         txt_area.innerHTML=item.innerHTML
+//     })
+//   })
 
 
+document.querySelectorAll('.triggerClass').forEach(item => {
+    item.addEventListener('click', event => {
+        txt_area.value=item.innerHTML
+
+    })
+  })
 
 
